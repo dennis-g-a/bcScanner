ace185b27141c86a7358f571db9cd0afd61030d5
dennis-g-a <dennisgallucciarboleas@gmail.com>
2025-01-10 14:35:00 -0800

    updated

--- js/camera.js
+++ js/camera.js
@@ -42,4 +42,43 @@ function getStreamCapabilities(stream){
 }
 
 
+function setResolution(capabilities, track, element){
+    element.innerHTML = '';
+    
+    for (const i of [1.0, 0.75, 0.50]){
+        const streamWidth = capabilities.width.max;
+        const aspectRatio =  track.getSettings().aspectRatio;
 
+        const label = `${Math.floor(streamWidth * i)}X${Math.floor((streamWidth * i) / aspectRatio)}`;
+
+        element.appendChild(new Option(label,label));
+    }
+}
+
+function setFocusMode(capabilities, element){
+    element.innerHTML = '';
+
+    if(!('focusMode' in capabilities)){
+        console.log('focusMode not available');
+        element.appendChild(new Option("None"))
+        element.disabled = true;
+    }else{
+        capabilities.focusMode.map(i=>{
+            element.appendChild(new Option(i,i));
+        })
+    }
+}
+
+function setFocusDistance(capabilities, element){
+    element.innerHTML = '';
+    
+    if(!('focusDistance' in capabilities)){
+        console.log('focusDistance not available');
+        element.disabled = true;
+    }else{
+        element.min = capabilities.focusDistance.min;
+        element.max = capabilities.focusDistance.max;
+        element.step = capabilities.focusDistance.step;
+        element.value = track.getSettings().focusDistance;
+    }
+}
